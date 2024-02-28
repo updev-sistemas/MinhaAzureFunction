768f928203763743f8fff8e8182374127f266dc6
Barros, Antonio <antonio.barros@studentambassador.com>
2024-02-28 17:09:53 -0300

    Minha Azure Function

---
+++ src/CalculadoraAF/Calculadora.cs
@@ -0,0 +1,59 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace SA;
+
+public class Calculadora
+{
+    private readonly ILogger<Calculadora> _logger;
+    
+    public Calculadora(ILogger<Calculadora> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("Calculadora")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        try {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string? name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonSerializer.Deserialize<dynamic>(requestBody!);
+            
+            name ??= data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "Esta função acionada por HTTP foi executada com sucesso. Passe um nome na string de consulta ou no corpo da solicitação para uma resposta personalizada."
+                : $"Olá, {name}. Espero que esse evento seja útil para você.";
+
+            var dataToResponse = new 
+            {
+                Message = responseMessage,
+                Date = DateTime.Now,
+                Application = "FunctionAppParaApresentar",
+                Version = "1.0.0",
+                Program = "Student Ambassadors - Microsoft - 2024"
+            };
+
+            return new OkObjectResult(dataToResponse);
+        }
+        catch (Exception ex) {
+            var dataToResponse = new 
+            {
+                Message = ex.Message,
+                Date = DateTime.Now,
+                Application = "FunctionAppParaApresentar",
+                Version = "1.0.0",
+                Program = "Student Ambassadors - Microsoft - 2024"
+            };
+
+            return new OkObjectResult(dataToResponse);
+        }
+    }
+}
